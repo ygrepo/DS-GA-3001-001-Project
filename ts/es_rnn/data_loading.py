0a98b3a858a3141064fa84a45c7558ffe46af81d
Yves Greatti <yvgrotti@gmail.com>
2019-11-11 09:13:08 -0500

    Added esrnn code

---
+++ ts/es_rnn/data_loading.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+
+def read_file(file_location, sampling=False, sample_size=5):
+    series = []
+    ids = dict()
+    with open(file_location, 'r') as file:
+        data = file.read().split("\n")
+
+    for i in range(1, len(data) - 1):
+        row = data[i].replace('"', '').split(',')
+        series.append(np.array([float(j) for j in row[1:] if j != ""]))
+        ids[row[0]] = i - 1
+        if sampling and i == sample_size:
+            series = np.array(series)
+            return series, ids
+
+    series = np.array(series)
+    return series, ids
+
+
+def create_val_set(train, output_size):
+    val = []
+    new_train = []
+    for i in range(len(train)):
+        val.append(train[i][-output_size:])
+        new_train.append(train[i][:-output_size])
+    return np.array(new_train), np.array(val)
+
+
+def chop_series(train, chop_val):
+    # CREATE MASK FOR VALUES TO BE CHOPPED
+    train_len_mask = [True if len(i) >= chop_val else False for i in train]
+    # FILTER AND CHOP TRAIN
+    train = [train[i][-chop_val:] for i in range(len(train)) if train_len_mask[i]]
+    return train, train_len_mask
+
+
+def create_datasets(train_file_location, test_file_location, output_size, sample=False, sampling_size=5):
+    train, train_idx = read_file(train_file_location, sample, sampling_size)
+    test, test_idx = read_file(test_file_location, sample, sampling_size)
+    train, val = create_val_set(train, output_size)
+    return train, train_idx, val, test, test_idx
+
+
+class SeriesDataset(Dataset):
+
+    def __init__(self, dataTrain, dataVal, dataTest, info, variable, chop_value, device, train_idx, sample=False):
+        dataTrain, mask = chop_series(dataTrain, chop_value)
+        if sample:
+            info = info[(info["M4id"].isin(train_idx.keys())) & (info["SP"] == variable)]
+        self.dataInfoCatOHE = pd.get_dummies(info[info["SP"] == variable]["category"])
+        self.dataInfoCatHeaders = np.array([i for i in self.dataInfoCatOHE.columns.values])
+        self.dataInfoCat = torch.from_numpy(self.dataInfoCatOHE[mask].values).float()
+        self.dataTrain = [torch.tensor(dataTrain[i]) for i in range(len(dataTrain))]  # ALREADY MASKED IN CHOP FUNCTION
+        self.dataVal = [torch.tensor(dataVal[i]) for i in range(len(dataVal)) if mask[i]]
+        self.dataTest = [torch.tensor(dataTest[i]) for i in range(len(dataTest)) if mask[i]]
+        self.device = device
+
+    def __len__(self):
+        return len(self.dataTrain)
+
+    def __getitem__(self, idx):
+        return self.dataTrain[idx].to(self.device), \
+               self.dataVal[idx].to(self.device), \
+               self.dataTest[idx].to(self.device), \
+               self.dataInfoCat[idx].to(self.device), \
+               idx
+
+
+def collate_lines(seq_list):
+    train_, val_, test_, info_cat_, idx_ = zip(*seq_list)
+    train_lens = [len(seq) for seq in train_]
+    seq_order = sorted(range(len(train_lens)), key=train_lens.__getitem__, reverse=True)
+    train = [train_[i] for i in seq_order]
+    val = [val_[i] for i in seq_order]
+    test = [test_[i] for i in seq_order]
+    info_cat = [info_cat_[i] for i in seq_order]
+    idx = [idx_[i] for i in seq_order]
+    return train, val, test, info_cat, idx
---
+++ ts/es_rnn/esrnn_main.py
@@ -0,0 +1,43 @@
+import os
+import time
+from pathlib import Path
+
+import pandas as pd
+from torch.utils.data import DataLoader
+
+from ts.es_rnn.config import get_config
+from ts.es_rnn.data_loading import create_datasets, SeriesDataset
+from ts.es_rnn.model import ESRNN
+from ts.es_rnn.trainer import ESRNNTrainer
+
+print("Start")
+try:
+    user_paths = os.environ['PYTHONPATH'].split(os.pathsep)
+    print(user_paths)
+except KeyError:
+    user_paths = []
+
+BASE_DIR = Path("/Users/yg/code/github/DS-GA-3001-001-Project/data/raw/")
+print('loading config')
+# config = get_config("Quarterly")
+config = get_config('Monthly')
+
+print('loading data')
+info = pd.read_csv(str(BASE_DIR / "M4info.csv"))
+
+train_path = str(BASE_DIR / "train/%s-train.csv") % (config["variable"])
+test_path = str(BASE_DIR / "test/%s-test.csv") % (config["variable"])
+
+sample = True
+train, train_idx, val, test, test_idx = create_datasets(train_path, test_path, config["output_size"], sample=sample)
+print(train.shape, val.shape, test.shape)
+
+dataset = SeriesDataset(train, val, test, info, config["variable"], config["chop_val"], config["device"],
+                        train_idx, sample)
+config["num_of_categories"] = len(dataset.dataInfoCatHeaders)
+dataloader = DataLoader(dataset, batch_size=config['batch_size'], shuffle=True)
+
+run_id = str(int(time.time()))
+model = ESRNN(num_series=len(dataset), config=config)
+tr = ESRNNTrainer(model, dataloader, run_id, config, ohe_headers=dataset.dataInfoCatHeaders)
+tr.train_epochs()
