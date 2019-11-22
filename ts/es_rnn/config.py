acd64ce957c485a216fc8778aa17032ed396670d
EC2 Default User <ec2-user@ip-10-207-48-185.aws-dscipt-np.nielsencsp.net>
2019-11-22 20:49:54 +0000

    Quarterly model

--- ts/es_rnn/config.py
+++ ts/es_rnn/config.py
@@ -15,8 +15,8 @@ def get_config(interval):
         "rnn_cell_type": "LSTM",
         "learning_rate": 1e-3,
         "learning_rates": ((10, 1e-4)),
-        "num_of_train_epochs": 2,
-        "num_of_train_epochs_sampling": 15,
+        "num_of_train_epochs": 8,
+        "num_of_train_epochs_sampling": 1,
         "num_of_categories": 6,  # in data provided
         "batch_size": 1024,
         "gradient_clipping": 20,
@@ -29,11 +29,11 @@ def get_config(interval):
         "print_output_stats": 3,
         "lr_anneal_rate": 0.5,
         "lr_anneal_step": 5,
-        "sample": True,
+        "sample": False,
         "reload": SAVE_LOAD_TYPE.NO_ACTION,
         "add_run_id": False,
-        "save_model": SAVE_LOAD_TYPE.NO_ACTION,
-        "plot_ts": True
+        "save_model": SAVE_LOAD_TYPE.MODEL,
+        "plot_ts": False
     }
 
     if interval == "Quarterly":
