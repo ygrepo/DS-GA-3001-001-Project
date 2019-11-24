Fixed validation computation loss for essrnn and WIP for benchmark

ts/abstract_trainer.py
@@ -16,16 +16,22 @@ class BaseTrainer(nn.Module):
                  sampling=False, reload=SAVE_LOAD_TYPE.NO_ACTION):
         super(BaseTrainer, self).__init__()
         self.model_name = model_name
-        self.model = model.to(config["device"])
+        if model:
+            self.model = model.to(config["device"])
+        else:
+            self.model = None
         self.config = config
         self.data_loader = dataloader
         self.sampling = sampling
         self.ohe_headers = ohe_headers
         self.optimizer = optimizer
         # self.optimizer = torch.optim.ASGD(self.model.parameters(), lr=config["learning_rate"])
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer,
-                                                         step_size=config["lr_anneal_step"],
-                                                         gamma=config["lr_anneal_rate"])
+        if optimizer:
+            self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer,
+                                                             step_size=config["lr_anneal_step"],
+                                                             gamma=config["lr_anneal_rate"])
+        else:
+            self.scheduler = None
         self.criterion = criterion
         # self.criterion = SmoothL1Loss()
         self.epochs = 0
@@ -54,21 +60,26 @@ class BaseTrainer(nn.Module):
         start_time = time.time()
         file_path = Path(".") / ("models/" + self.model_name)
         if self.reload == SAVE_LOAD_TYPE.MODEL:
-            self.model = load_model(file_path)
+            model, optimizer = load_model(file_path, self.config)
+            self.model = model
+            self.optimizer = optimizer
         if self.reload == SAVE_LOAD_TYPE.MODEL_PARAMETERS:
             load_model_parameters(file_path, self.model, self.optimizer)
         max_loss_repeat = 3
         loss_repeat_counter = 1
         prev_loss = float("-inf")
         for e in range(self.max_epochs):
-            epoch_loss = self.train()
-            #epoch_loss = 0
+
+            if self.reload != SAVE_LOAD_TYPE.NO_ACTION:
+                epoch_loss = 0
+            else:
+                epoch_loss = self.train()
 
             if self.save_model_enabled() and epoch_loss < max_loss:
                 print("Loss decreased, saving model!")
                 file_path = Path(".") / ("models/" + self.model_name)
                 if self.config["save_model"] == SAVE_LOAD_TYPE.MODEL:
-                    save_model(file_path, self.model, self.run_id)
+                    save_model(file_path, self.model, self.optimizer, self.run_id, self.add_run_id)
                 elif self.config["save_model"] == SAVE_LOAD_TYPE.MODEL_PARAMETERS:
                     save_model_parameters(file_path, self.model, self.optimizer, self.run_id, self.add_run_id)
                 max_loss = epoch_loss
@@ -77,7 +88,8 @@ class BaseTrainer(nn.Module):
                 loss_repeat_counter += 1
                 if loss_repeat_counter >= max_loss_repeat:
                     print("Loss not decreasing for last {} times".format(loss_repeat_counter))
-                    if self.model_name == MODEL_TYPE.NBEATS.value and self.plot_ts_enabled() and self.config["sample_ids"]:
+                    if self.model_name == MODEL_TYPE.NBEATS.value and self.plot_ts_enabled() and self.config[
+                        "sample_ids"]:
                         plot_stacks(self.run_id, self.figure_path, self.model)
                     break
                 else:
@@ -101,7 +113,8 @@ class BaseTrainer(nn.Module):
         print("Total Training in mins: %5.2f" % ((time.time() - start_time) / 60))
 
     def train(self):
-        self.model.train()
+        if self.model:
+            self.model.train()
         epoch_loss = 0
         for batch_num, (train, val, test, info_cat, _, idx) in enumerate(self.data_loader):
             start = time.time()

ts/benchmark/config.py
@@ -2,6 +2,7 @@ from math import sqrt
 
 import torch
 
+from ts.n_beats.model import BLOCK_TYPE
 from ts.utils.helper_funcs import SAVE_LOAD_TYPE
 
 
@@ -11,16 +12,13 @@ def get_config(interval):
         "device": ("cuda" if torch.cuda.is_available() else "cpu"),
         "percentile": 50,
         "training_percentile": 45,
-        "add_nl_layer": True,
-        "rnn_cell_type": "LSTM",
         "learning_rate": 1e-3,
         "learning_rates": ((10, 1e-4)),
-        "num_of_train_epochs": 1,
+        "num_of_train_epochs": 100,
         "num_of_train_epochs_sampling": 1,
         "num_of_categories": 6,  # in data provided
         "batch_size": 1024,
         "gradient_clipping": 20,
-        "c_state_penalty": 0,
         "min_learning_rate": 0.0001,
         "lr_ratio": sqrt(10),
         "lr_tolerance_multip": 1.005,
@@ -29,96 +27,97 @@ def get_config(interval):
         "print_output_stats": 3,
         "lr_anneal_rate": 0.5,
         "lr_anneal_step": 5,
-        "sample": True,
-        "reload": SAVE_LOAD_TYPE.MODEL,
+        "sample": False,
+        "reload": SAVE_LOAD_TYPE.NO_ACTION,
         "add_run_id": False,
         "save_model": SAVE_LOAD_TYPE.NO_ACTION,
-        "plot_ts": True
+        "plot_ts": True,
     }
 
     if interval == "Quarterly":
         config.update({
-            "chop_val": 72,
+            "stack_types": [BLOCK_TYPE.TREND, BLOCK_TYPE.SEASONALITY],
+            "thetas_dims": [2, 8],
+            "nb_blocks_per_stack": 3,
+            "hidden_layer_units": 128,
+            "share_weights_in_stack": False,
             "variable": "Quarterly",
-            "dilations": ((1, 2), (4, 8)),
-            "state_hsize": 40,
             "seasonality": 4,
-            "input_size": 4,
             "output_size": 8,
-            "level_variability_penalty": 80,
             #"sample_ids": [],
-            "sample_ids": ["Q66"],
+            "sample_ids": ["Q24000"],
         })
     elif interval == "Monthly":
         config.update({
+            "stack_types": [BLOCK_TYPE.TREND, BLOCK_TYPE.SEASONALITY],
+            "thetas_dims": [2, 8],
+            "nb_blocks_per_stack": 3,
+            "hidden_layer_units": 128,
+            "share_weights_in_stack": True,
             "chop_val": 72,
             "variable": "Monthly",
-            "dilations": ((1, 3), (6, 12)),
-            "state_hsize": 50,
             "seasonality": 12,
-            "input_size": 12,
             "output_size": 18,
-            "level_variability_penalty": 50,
-            "sample_ids": [],
-            # "sample_ids": ["M1"],
+            # "sample_ids": [],
+            "sample_ids": ["M1"],
         })
     elif interval == "Daily":
         config.update({
-            "chop_val": 200,
+            "stack_types": [BLOCK_TYPE.TREND, BLOCK_TYPE.SEASONALITY],
+            "thetas_dims": [2, 8],
+            "nb_blocks_per_stack": 3,
+            "hidden_layer_units": 128,
+            "share_weights_in_stack": True,
             "variable": "Daily",
-            "dilations": ((1, 7), (14, 28)),
-            "state_hsize": 50,
             "seasonality": 7,
-            "input_size": 7,
             "output_size": 14,
-            "level_variability_penalty": 50,
-            # "sample_ids": [],
-            "sample_ids": ["D1"],
+            #"sample_ids": [],
+            "sample_ids": ["D404"],
         })
     elif interval == "Yearly":
 
         config.update({
-            "chop_val": 25,
+            "stack_types": [BLOCK_TYPE.TREND, BLOCK_TYPE.SEASONALITY],
+            "thetas_dims": [3, 8],
+            "nb_blocks_per_stack": 3,
+            "hidden_layer_units": 128,
+            "share_weights_in_stack": True,
             "variable": "Yearly",
-            "dilations": ((1, 2), (2, 6)),
-            "state_hsize": 30,
             "seasonality": 1,
-            "input_size": 4,
             "output_size": 6,
-            "level_variability_penalty": 0,
             "sample_ids": [],
-            # "sample_ids": ["Y3974"],
+            #"sample_ids": ["Y3974"],
         })
     elif interval == "Weekly":
         config.update({
-            "chop_val": 25,
+            "stack_types": [BLOCK_TYPE.TREND, BLOCK_TYPE.SEASONALITY],
+            "thetas_dims": [2, 8],
+            "nb_blocks_per_stack": 3,
+            "hidden_layer_units": 128,
+            "share_weights_in_stack": True,
             "variable": "Weekly",
-            "dilations": ((1, 14), (14, 28)),
-            "state_hsize": 60,
             "seasonality": 1,
-            "input_size": 1,
             "output_size": 13,
-            "level_variability_penalty": 0,
-            # "sample_ids": [],
-            "sample_ids": ["W1"],
+            #"sample_ids": [],
+             "sample_ids": ["W246"],
         })
     elif interval == "Hourly":
         config.update({
-            "chop_val": 25,
+            "stack_types": [BLOCK_TYPE.TREND, BLOCK_TYPE.SEASONALITY],
+            "thetas_dims": [2, 8],
+            "nb_blocks_per_stack": 3,
+            "hidden_layer_units": 128,
+            "share_weights_in_stack": False,
             "variable": "Hourly",
-            "dilations": ((1, 24), (24, 48)),
-            "state_hsize": 60,
             "seasonality": 24,
-            "input_size": 24,
             "output_size": 48,
-            "level_variability_penalty": 0,
             "sample_ids": [],
-            # "sample_ids": ["H344"],
+            #"sample_ids": ["H344"],
         })
     else:
-        print("I don\"t have that config. :(")
+        print("I dont have that config. :(")
 
-    config["input_size_i"] = config["input_size"]
+    # config["input_size_i"] = config["input_size"]
     config["output_size_i"] = config["output_size"]
     config["tau"] = config["percentile"] / 100
     config["training_tau"] = config["training_percentile"] / 100

ts/benchmark/main.py
@@ -1,14 +1,18 @@
 import time
 from pathlib import Path
 
+import gpytorch
 import pandas as pd
+import torch
 from torch.utils.data import DataLoader
 
-from ts.n_beats.config import get_config
+from ts.benchmark.config import get_config
+from ts.benchmark.model import SpectralMixtureGPModel
+from ts.benchmark.trainer import Trainer
 from ts.utils.data_loading import SeriesDataset
-from ts.n_beats.model import NBeatsNet
-from ts.n_beats.trainer import Trainer
-from ts.utils.helper_funcs import MODEL_TYPE, set_seed, create_datasets
+from ts.utils.helper_funcs import MODEL_TYPE, set_seed, create_datasets, generate_timeseries_length_stats, \
+    filter_timeseries, determine_chop_value
+from ts.utils.loss_modules import PinballLoss
 
 
 def main():
@@ -24,8 +28,6 @@ def main():
     print("Loading config")
     config = get_config("Monthly")
     print("Frequency:{}".format(config["variable"]))
-    forecast_length = config["output_size"]
-    backcast_length = 1 * forecast_length
 
     print("loading data")
     info = pd.read_csv(str(BASE_DIR / "M4info.csv"))
@@ -34,12 +36,31 @@ def main():
 
     sample = config["sample"]
     sample_ids = config["sample_ids"] if "sample_ids" in config else []
-    train, ts_labels, val, test, test_idx = create_datasets(train_path, test_path, config["output_size"],
+    train, ts_labels, _, test, test_idx = create_datasets(train_path, test_path, config["output_size"],
+                                                            create_val_dataset=False,
                                                             sample_ids=sample_ids, sample=sample,
                                                             sampling_size=4)
-    print("#.train:{}, #.validation ts:{}, #.test ts:{}".format(len(train), len(val), len(test)))
-
+    generate_timeseries_length_stats(train)
+    print("#.train:{}, #.test ts:{}".format(len(train), len(test)))
+    reload = config["reload"]
+    add_run_id = config["add_run_id"]
+    criterion = PinballLoss(config["training_tau"], config["output_size"] * config["batch_size"], config["device"])
+    trainer = Trainer(MODEL_TYPE.BENCHMARK.value, None, None, criterion, run_id, add_run_id, config,
+                      csv_path=LOG_DIR, figure_path=FIGURE_PATH,
+                      sampling=sample, reload=reload)
+    trainer.train_epochs()
 
 
 if __name__ == "__main__":
+    # Training data is 11 points in [0,1] inclusive regularly spaced
+    # train_x = torch.linspace(0, 1, 100).view(1, -1, 1).repeat(4, 1, 1)
+    # # True function is sin(2*pi*x) with Gaussian noise
+    # import math
+    # sin_y = torch.sin(train_x[0] * (2 * math.pi)) + 0.5 * torch.rand(1, 100, 1)
+    # sin_y_short = torch.sin(train_x[0] * (math.pi)) + 0.5 * torch.rand(1, 100, 1)
+    # cos_y = torch.cos(train_x[0] * (2 * math.pi)) + 0.5 * torch.rand(1, 100, 1)
+    # cos_y_short = torch.cos(train_x[0] * (math.pi)) + 0.5 * torch.rand(1, 100, 1)
+    # train_y = torch.cat((sin_y, sin_y_short, cos_y, cos_y_short)).squeeze(-1)
+    #
+    # print(train_x.shape, train_y.shape)
     main()

ts/benchmark/trainer.py
@@ -8,22 +8,35 @@ import torch.nn as nn
 from ts.abstract_trainer import BaseTrainer
 from ts.utils.loss_modules import np_sMAPE, np_MASE, np_mase
 from gpytorch import mlls
+import gpytorch
+from ts.benchmark.model import SpectralMixtureGPModel
 
 
 class Trainer(BaseTrainer):
-    def __init__(self, model_name, model, optimizer, criterion, dataloader, run_id, add_run_id, config,
-                 ohe_headers, csv_path, figure_path, sampling, reload):
+    def __init__(self, model_name, model, optimizer, criterion, dataloader, run_id, add_run_id, config, ohe_headers,
+                 csv_path, figure_path,
+                 sampling, reload):
         super().__init__(model_name, model, optimizer, criterion, dataloader, run_id, add_run_id, config, ohe_headers,
                          csv_path, figure_path, sampling, reload)
 
     def train_batch(self, train, val, test, info_cat, idx):
 
-        self.model.covar_module.initialize_from_data(train, val)
+        #self.model.covar_module.initialize_from_data(train, val)
+        y_train = train.view(1,-1,1)
+        x_train = np.array(range(0, train.shape[1]))
+        #val = val.view(1,-1,1)
+        likelihood = gpytorch.likelihoods.GaussianLikelihood(batch_size=y_train.shape[1])
+        self.model = SpectralMixtureGPModel(x_train, y_train, likelihood, num_outputs=y_train.shape[1])
         self.model.train()
         self.model.likelihood.train()
         self.mll = mlls.ExactMarginalLogLikelihood(self.model.likelihood, self.model)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config["learning_rate"])
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer,
+                                                         step_size=self.config["lr_anneal_step"],
+                                                         gamma=self.config["lr_anneal_rate"])
 
         self.optimizer.zero_grad()
+        print(x_train.shape, y_train.shape)
         forecast = self.model(train)
         loss = -self.mll(forecast, val)
         loss.backward()
@@ -33,6 +46,7 @@ class Trainer(BaseTrainer):
 
     def val(self, file_path, testing=False):
         self.model.eval()
+        self.likelihood.eval()
         with torch.no_grad():
             acts = []
             preds = []

ts/es_rnn/main.py
@@ -12,7 +12,7 @@ from ts.es_rnn.model import ESRNN
 from ts.es_rnn.trainer import ESRNNTrainer
 from ts.utils.data_loading import SeriesDataset
 from ts.utils.helper_funcs import MODEL_TYPE, set_seed, create_datasets, generate_timeseries_length_stats, \
-    filter_timeseries
+    filter_timeseries, SAVE_LOAD_TYPE
 
 set_seed(0)
 
@@ -62,10 +62,10 @@ dataset = SeriesDataset(data_infocat_ohe, data_infocat_headers, data_info_cat, t
 config["num_of_categories"] = len(dataset.data_info_cat_headers)
 dataloader = DataLoader(dataset, batch_size=config["batch_size"], shuffle=True)
 
-model = ESRNN(num_series=len(dataset), config=config)
 reload = config["reload"]
-add_run_id = config["add_run_id"]
+model = ESRNN(num_series=len(dataset), config=config)
 optimizer = torch.optim.Adam(model.parameters(), lr=config["learning_rate"])
+add_run_id = config["add_run_id"]
 # criterion = PinballLoss(config["training_tau"], config["output_size"] * config["batch_size"], config["device"])
 criterion = SmoothL1Loss()
 tr = ESRNNTrainer(MODEL_TYPE.ESRNN.value, model, optimizer, criterion, dataloader, run_id, add_run_id, config,

ts/es_rnn/model.py
@@ -102,8 +102,8 @@ class ESRNN(nn.Module):
             input_window_start = i + 1 - self.config["input_size"]
             input_window_end = i + 1
 
-            train_deseas_window_input = train[:, input_window_start:input_window_end] / seasonalities_stacked[:,
-                                                                                        input_window_start:input_window_end]
+            train_deseas_window_input = train[:, input_window_start:input_window_end] / \
+                                        seasonalities_stacked[:,input_window_start:input_window_end]
             train_deseas_norm_window_input = (train_deseas_window_input / levs_stacked[:, i].unsqueeze(1))
             train_deseas_norm_cat_window_input = torch.cat((train_deseas_norm_window_input, info_cat), dim=1)
             window_input_list.append(train_deseas_norm_cat_window_input)

ts/es_rnn/trainer.py
@@ -45,9 +45,10 @@ class ESRNNTrainer(BaseTrainer):
                 _, _, (hold_out_pred, network_output_non_train), \
                 (hold_out_act, hold_out_act_deseas_norm), _ = self.model(train, val, test, info_cat, idx, testing=True)
                 # Compute loss between normalized and deseasonalized predictions and
-                # either validation or test data depending the value of the flag testing.
-                hold_out_loss += self.criterion(network_output_non_train.unsqueeze(0).float(),
-                                                hold_out_act_deseas_norm.unsqueeze(0).float())
+                # either validation or test data depending the value of the flag testing
+                # hold_out_loss += self.criterion(network_output_non_train.unsqueeze(0).float(),
+                #                                 hold_out_act_deseas_norm.unsqueeze(0).float())
+                hold_out_loss += self.criterion(hold_out_pred, hold_out_act)
                 acts.extend(hold_out_act.view(-1).cpu().detach().numpy())
                 total_act = torch.cat((train, hold_out_pred), dim=1)
                 total_acts.extend(total_act.view(-1).cpu().detach().numpy())

ts/utils/helper_funcs.py
@@ -50,7 +50,7 @@ def unpad_sequence(padded_sequence, lens):
     return seqs
 
 
-def save_model(file_path, model, run_id, add_run_id=False):
+def save_model(file_path, model, optimizer, run_id, add_run_id=False):
     file_path.mkdir(parents=True, exist_ok=True)
     if add_run_id:
         model_path = file_path / ("model_" + run_id + ".pyt")
@@ -58,18 +58,26 @@ def save_model(file_path, model, run_id, add_run_id=False):
         model_path = file_path / ("model.pyt")
 
     torch.save(model, model_path)
+    print("Saving whole model and optimizer state dictionary")
+    torch.save({
+        "model": model,
+        "optimizer_state_dict": optimizer.state_dict(),
+    }, model_path)
 
 
-def load_model(file_path):
+def load_model(file_path, config):
     model_path = file_path / "model.pyt"
     if model_path.exists():
         if torch.cuda.is_available():
             map_location = lambda storage, loc: storage.cuda()
         else:
             map_location = "cpu"
-        model = torch.load(model_path, map_location=map_location)
-        print(f"Restored checkpoint from {model_path}.")
-        return model
+        checkpoint = torch.load(model_path, map_location=map_location)
+        model = checkpoint["model"]
+        optimizer = torch.optim.Adam(model.parameters(), lr=config["learning_rate"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        print(f"Restored checkpoint(whole model and optimizer state dictionary) from {model_path}.")
+        return model, optimizer
 
 
 def save_model_parameters(file_path, model, optimiser, run_id, add_run_id=False):
@@ -79,6 +87,7 @@ def save_model_parameters(file_path, model, optimiser, run_id, add_run_id=False)
     else:
         model_path = file_path / ("model.pyt")
 
+    print("Saving mode and optimizer state dictionaries")
     torch.save({
         "model_state_dict": model.state_dict(),
         "optimizer_state_dict": optimiser.state_dict(),
@@ -95,7 +104,7 @@ def load_model_parameters(file_path, model, optimiser):
         checkpoint = torch.load(model_path, map_location=map_location)
         model.load_state_dict(checkpoint["model_state_dict"])
         optimiser.load_state_dict(checkpoint["optimizer_state_dict"])
-        print(f"Restored checkpoint from {model_path}.")
+        print(f"Restored checkpoint(state dictionaries) from {model_path}.")
 
 
 def read_file(file_location, sample_ids, sampling=False, sample_size=5):
@@ -138,7 +147,9 @@ def create_val_set(train, output_size):
     return np.array(new_train), np.array(val)
 
 
-def create_datasets(train_file_location, test_file_location, output_size, sample_ids, sample=False, sampling_size=5):
+def create_datasets(train_file_location, test_file_location, output_size,
+                    create_val_dataset=True,
+                    sample_ids=[], sample=False, sampling_size=5):
     train, train_idx = read_file(train_file_location, sample_ids, sample, sampling_size)
     if sample and sample_ids:
         train, train_idx = filter_sample_ids(train, train_idx, sample_ids)
@@ -148,7 +159,11 @@ def create_datasets(train_file_location, test_file_location, output_size, sample
     if sample and sample_ids:
         test, test_idx = filter_sample_ids(test, test_idx, sample_ids)
 
-    train, val = create_val_set(train, output_size)
+    if create_val_dataset:
+        train, val = create_val_set(train, output_size)
+    else:
+        val = None
+
     if sample and sample_ids:
         print("Sampling train data for {}".format(sample_ids))
         print("Sampling test data for {}".format(sample_ids))
@@ -176,7 +191,7 @@ def determine_chop_value(data, backcast_length, forecast_length):
             ts_lengths.append(len(ts))
         # print(len(ts), length)
     if ts_lengths:
-        #return np.quantile(ts_lengths, 0.25).astype(dtype=int)
+        # return np.quantile(ts_lengths, 0.25).astype(dtype=int)
         return np.amin(np.array(ts_lengths)).astype(dtype=int)
     return -1
 
@@ -189,7 +204,7 @@ def chop_series(train, chop_val):
     return train, train_len_mask
 
 
-def filter_timeseries(info,variable, sample, ts_labels, data_train, chop_val, data_val, data_test):
+def filter_timeseries(info, variable, sample, ts_labels, data_train, chop_val, data_val, data_test):
     data_train, mask = chop_series(data_train, chop_val)
     if sample:
         info = info[(info["M4id"].isin(ts_labels.keys())) & (info["SP"] == variable)]